f6c775c100f0e578777fb0d4084b3ddadd97a019
Frank Nord <frank.nord@mailbox.org>
2016-01-25 21:26:43 +0100

    Misc Fixes (2)

--- supernode/06c_tinc.sh
+++ supernode/06c_tinc.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
+
+# Patch erstellen
 # Git konfigurieren - Anpassen (!)
 git config --global user.name "Frank Nord"
 git config --global user.email "frank.nord@mailbox.org"
 
-# Public Key erstellen
-tincd -n kbubackbone -K 
-
-# Patch erstellen
 cd /etc/tinc/kbubackbone/hosts/
 git add mein_neuer_supernode
 git commit -a -m "Added mein_neuer_supernode"
 git format-patch HEAD~..HEAD -o /tmp/
 cat /tmp/0001-Added-mein_neuer_supernode.patch
 # /tmp/0001-Added-mein_neuer_supernode.patch
-
--- supernode/06d_tinc.sh
+++ supernode/06d_tinc.sh
@@ -1,24 +1,13 @@
 #!/bin/sh
 cat > /etc/tinc/kbubackbone/tinc-up <<EOF
-    #!/bin/sh
-    ip addr add 172.27.8.1/32 dev \$INTERFACE
-    ip route add 172.27.8.0/25 dev br-freifunk table 66 
-    ip route add 172.26.0.0/15 dev \$INTERFACE table 66 
-    ip route add 10.158.0.0/15 dev \$INTERFACE table 66 
-    ip route add 172.26.0.0/15 dev \$INTERFACE 
-    ip route add 10.158.0.0/15 dev \$INTERFACE 
-    ip route add default dev \$INTERFACE table 66 
+#!/bin/sh
+ifconfig 172.27.8.1 netmask 255.255.255.255 \$INTERFACE up
+ip route add 172.26.0.0/15 dev \$INTERFACE table 66 
+ip route add 10.158.0.0/15 dev \$INTERFACE table 66 
+ip route add 172.26.0.0/15 dev \$INTERFACE 
+ip route add 10.158.0.0/15 dev \$INTERFACE 
+ip route add default dev \$INTERFACE table 66 
+ip route add 172.27.8.0/25 dev br-freifunk table 66 
 EOF
 
-cat > /etc/tinc/kbubackbone/tinc-down <<EOF
-    #!/bin/sh
-    ip route del 172.27.8.0/25 dev br-freifunk table 66 
-    ip route del 172.26.0.0/15 dev \$INTERFACE table 66 
-    ip route del 10.158.0.0/15 dev \$INTERFACE table 66 
-    ip route del 172.26.0.0/15 dev \$INTERFACE 
-    ip route del 10.158.0.0/15 dev \$INTERFACE 
-    ip route del default dev \$INTERFACE table 66 
-EOF
-chmod 755 /etc/tinc/kbubackbone/tinc-up
-chmod 755 /etc/tinc/kbubackbone/tinc-down
-
+chmod 755 /etc/tinc/kbubackbone/tinc-ups
